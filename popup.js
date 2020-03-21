8b254a0506b8b1f2b6eaef3830d1a411ac987952
ashutoshdumiyan <ashutosh.dumiyan@gmail.com>
2020-03-21 20:23:09 +0530

    increased searching accuracy

--- popup.js
+++ popup.js
@@ -9,15 +9,15 @@ chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, { data: "added script" }, function(
         response
       ) {
+        // console.log(response);
         if (response.error) {
           console.log("Song not playing");
-        } else {
+        } else if (response.touse == 1) {
           axios
             .get(
-              `https://orion.apiseeds.com/api/music/lyric/${response.artist}/${response.song}?apikey=${key}`
+              `https://orion.apiseeds.com/api/music/lyric/${response.first.artist}/${response.first.song}?apikey=${key}`
             )
             .then(result => {
-              // console.log(result);
               if (result.status == 404) {
                 console.log("Lyrics not found");
               } else if (result.status == 200) {
@@ -26,6 +26,47 @@ chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
               } else {
                 console.log("Some error occurred. Try again.");
               }
+            })
+            .catch(err => {
+              console.log("Lyrics not found");
+              axios
+                .get(
+                  `https://orion.apiseeds.com/api/music/lyric/${response.first.song}/${response.first.artist}?apikey=${key}`
+                )
+                .then(result => {
+                  console.log(result);
+                })
+                .catch(err => {
+                  console.log("Not found in retry");
+                });
+            });
+        } else if (response.touse == 2) {
+          axios
+            .get(
+              `https://orion.apiseeds.com/api/music/lyric/${response.second.artist2}/${response.second.song2}?apikey=${key}`
+            )
+            .then(result => {
+              if (result.status == 404) {
+                console.log("Lyrics not found");
+              } else if (result.status == 200) {
+                console.log("Lyrics found");
+                console.log(result.data.result);
+              } else {
+                console.log("Some error occurred. Try again.");
+              }
+            })
+            .catch(err => {
+              console.log("Lyrics not found");
+              axios
+                .get(
+                  `https://orion.apiseeds.com/api/music/lyric/${response.second.song2}/${response.second.artist2}?apikey=${key}`
+                )
+                .then(result => {
+                  console.log(result);
+                })
+                .catch(err => {
+                  console.log("Not found in retry");
+                });
             });
         }
       });
--- youtubeScript.js
+++ youtubeScript.js
@@ -18,26 +18,66 @@ if (window.alreadyDone === undefined) {
       title = title.replace("HD", " ");
       title = title.trim();
       title = title.toLowerCase();
-      let arr = title.split(" - ");
-      arr.forEach((val, ind) => {
-        if (val.indexOf("ft.") !== -1) {
-          arr[ind] = val.substring(0, val.indexOf("ft.")).trim();
-        } else if (val.indexOf("feat.") !== -1) {
-          arr[ind] = val.substring(0, val.indexOf("feat.")).trim();
+      let arr, arr2;
+      let touse = 1;
+      if (title.indexOf("-") !== -1) {
+        arr = title.split(" - ");
+        arr.forEach((val, ind) => {
+          if (val.indexOf("ft.") !== -1) {
+            arr[ind] = val.substring(0, val.indexOf("ft.")).trim();
+          } else if (val.indexOf("feat.") !== -1) {
+            arr[ind] = val.substring(0, val.indexOf("feat.")).trim();
+          }
+        });
+        let artist, song, mode;
+        if (arr.length === 1) {
+          artist = document
+            .querySelector(".yt-user-info a")
+            .textContent.toLowerCase()
+            .replace("music", "")
+            .replace(" - topic", "");
+          song = arr[0];
+          mode = "channel";
+        } else {
+          artist = arr[0];
+          song = arr[1];
+          mode = "title";
         }
-      });
-      let artist, song, mode;
-      if (arr.length === 1) {
-        artist = document.querySelector(".yt-user-info a").textContent;
-        song = arr[0];
-        mode = "channel";
-      } else {
-        artist = arr[0];
-        song = arr[1];
-        mode = "title";
+        console.log(artist, song);
+        sendResponse({
+          first: { artist: artist, song: song, mode: mode },
+          touse: touse
+        });
+      } else if (title.indexOf(":") !== -1) {
+        arr2 = title.split(":");
+        touse = 2;
+        arr2.forEach((value, index) => {
+          if (value.indexOf("ft.") !== -1) {
+            arr2[index] = value.substring(0, value.indexOf("ft.")).trim();
+          } else if (value.indexOf("feat.") !== -1) {
+            arr2[index] = value.substring(0, value.indexOf("feat.")).trim();
+          }
+        });
+        let artist2, song2, mode2;
+        if (arr2.length === 1) {
+          artist2 = document
+            .querySelector(".yt-user-info a")
+            .textContent.toLowerCase()
+            .replace("music", "")
+            .replace(" - topic", "");
+          song2 = arr2[0];
+          mode2 = "channel";
+        } else {
+          artist2 = arr2[0];
+          song2 = arr2[1];
+          mode2 = "title";
+        }
+        console.log(artist2, song2);
+        sendResponse({
+          second: { artist2: artist2, song2: song2, mode2: mode2 },
+          touse: touse
+        });
       }
-      console.log(artist, song);
-      sendResponse({ artist: artist, song: song, mode: mode });
     }
   });
 }
